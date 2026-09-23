e983aa179a2bb1a887160d7c7cc15c954c4d6be6
Francis <francisuk1989@gmail.com>
2026-09-24 00:00:05 +0100

    OpenWrt wlNoiseFloor.sh to improve noise floor retrieval
    Replacement for listing associated clients.
    
    Tested on OpenWrt 25.12.5
    
    root@MR8300:/tmp# ./wlNoiseFloor.sh phy0-ap0
    -100
    root@MR8300:/tmp# ./wlNoiseFloor.sh phy1-ap0
    -81
    root@MR8300:/tmp# ./wlNoiseFloor.sh phy2-ap0
    -104

--- snmp/Openwrt/wlNoiseFloor.sh
+++ snmp/Openwrt/wlNoiseFloor.sh
@@ -2,7 +2,7 @@
 
 # wlNoiseFloor.sh
 # Returns wlNoiseFloor, in dBm
-# Arguments: targed interface
+# Arguments: targeted interface
 
 # Check number of arguments
 if [ $# -ne 1 ]; then
@@ -11,9 +11,9 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
-# Extract noise floor. Note, all associated stations have the same value, so just grab the first one
-# Use tail, not head (i.e. last line, not first), as head exits immediately, breaks the pipe to cut!
-noise=$(/usr/bin/iwinfo "$1" assoclist 2>/dev/null | grep -v "^$" | /usr/bin/cut -s -d "/" -f 2 | /usr/bin/cut -s -d "(" -f 1 | /usr/bin/cut -s -d " " -f 2 | /usr/bin/tail -1)
+# Use awk to find the "[in use]" line, set a flag, and then print the very next "noise:" value it sees.
+# This avoids using 'grep -A' which can be unreliable in embedded router environments.
+noise=$(iw dev "$1" survey dump 2>/dev/null | awk '/\[in use\]/{flag=1} flag && /noise:/{print $2; exit}')
 
 # Return snmp result
 /bin/echo "$noise"
